15cbd1eaab6c8604900391e0541d1e9a5cfdc3b1
Richard Rodger <richard@ricebridge.com>
2015-05-11 16:20:03 +0100

    it

--- service/salestax-country-main.js
+++ service/salestax-country-main.js
@@ -1,9 +1,9 @@
-var WEB_PORT = process.env.WEB_PORT || 51001
+var WEB_PORT = process.env.WEB_PORT || 51002
 var WEB_HOST = process.env.WEB_HOST || 'localhost'
 var BS_PORT  = process.env.BS_PORT  || 11300
 var BS_HOST  = process.env.BS_HOST  || 'localhost'
 
-var rates = {ie:0.21, uk:0.20, de:0.19}
+var rates = {it:0.22, ie:0.21, uk:0.20, de:0.19}
 
 require('seneca')()
   .use('..', rates )
