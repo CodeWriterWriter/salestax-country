9742e9d0c6f3e98a67ceb1ce5b118bf173e7659b
Richard Rodger <richard@ricebridge.com>
2015-05-11 15:28:12 +0100

    beanstalk

---
+++ service/salestax-country-main.js
@@ -0,0 +1,21 @@
+var WEB_PORT = process.env.WEB_PORT || 51001
+var WEB_HOST = process.env.WEB_HOST || 'localhost'
+var BS_PORT  = process.env.BS_PORT  || 11300
+var BS_HOST  = process.env.BS_HOST  || 'localhost'
+
+var rates = {ie:0.21, uk:0.20, de:0.19}
+
+require('seneca')()
+  .use('..', rates )
+
+  .use('beanstalk-transport')
+  .listen({
+    type: 'beanstalk',
+    pin:  'role:salestax,cmd:calculate,country:*',
+    port:  BS_PORT,
+    host:  BS_HOST  
+  })
+  .listen({
+    port:WEB_PORT, 
+    host:WEB_HOST
+  })
